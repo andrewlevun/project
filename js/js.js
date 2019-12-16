3e1a50c6caa9d35c6d8583c10687ac122c0e06fc
andrew levun <andrewlevun@gmail.com>
2019-12-16 11:35:09 +0300

    add files

---
+++ js/js.js
@@ -0,0 +1,12 @@
+
+  
+   /*$('body').flipLightBox
+   ({ lightbox_flip_speed: 500, 
+      lightbox_border_color: '#666666' 
+   })*/
+
+   $(document).ready(function(){
+      $('.nivo-lightbox a').nivoLightbox({ effect: 'fade' });
+      });
+  
+   
